5d073f0f03f1280b2a01994f61f26f2426d3af61
Zinarch <Sebastianlfransson@gmail.com>
2023-11-14 00:28:36 +0100

    [MOD-2] Add Mod Skeleton

---
+++ FirstMod/Settings.cs
@@ -0,0 +1,16 @@
+﻿using UnityModManagerNet;
+
+namespace FirstMod
+{
+    public class Settings : UnityModManager.ModSettings
+    {
+        public float MyFloatOption = 2f;
+        public bool MyBoolOption = true;
+        public string MyTextOption = "Hello";
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+    }
+}
